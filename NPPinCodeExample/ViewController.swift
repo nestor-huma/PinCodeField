0fa240c916bd1bf12f8873ba366f5249d6aa5d38
Nestor Popko <nestorpopko@gmail.com>
2016-02-14 21:28:34 +0200

    Initial Commit

---
+++ NPPinCodeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NPPinCodeExample
+//
+//  Created by Admin on 2/14/16.
+//  Copyright © 2016 Nestor Popko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
